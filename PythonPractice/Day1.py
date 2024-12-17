dcb5c5426a4256eda6e7439a7b0c5edd49ae2df7
SoyMakko <programacionmakko@gmail.com>
2024-12-17 09:56:21 -0600

    Add 100 days of practice module from replit

---
+++ PythonPractice/Day1.py
@@ -0,0 +1,11 @@
+# Text output
+
+print("Hello, World!")
+
+print(""" With 3 sets of quotes 
+      yoy can output the text as you 
+      l
+      i
+      k
+      e
+      !""")
---
+++ PythonPractice/Day2.py
@@ -0,0 +1,10 @@
+# Input and Variables
+
+name = input("Write your name: ")
+
+age = input("Age: ")
+
+print(f"Your name is {name} and your age is {age}!") # f string in python
+
+print(" \n Form 2 of printing: Your name is {} and your age is {}!".format(name,age)) 
+
