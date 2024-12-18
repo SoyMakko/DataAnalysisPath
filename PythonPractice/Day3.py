c0087b00f74b2b260f294a0b542b0d3c89660ab0
SoyMakko <programacionmakko@gmail.com>
2024-12-18 09:35:35 -0600

    Add day3 challenge

---
+++ PythonPractice/Day3.py
@@ -0,0 +1,10 @@
+food = input("Name a food")
+plant = input("Name a type of plant")
+cook = input("Name a method of cooking")
+Bfood = input("What word do you use to describe burned food?")
+item = input("Name a DIY item")
+
+print(f""" MENU
+    {cook} {food} with {Bfood} {plant} on a bed of {item}
+    """)
+
